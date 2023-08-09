562ee24e7d3c29e570a8d24d63783afb53efe2f8
ArgentoAskia <askiaxiaoming@outlook.com>
2023-08-09 16:49:11 +0800

    3-2023.8.9: Simple Start For Spring

---
+++ spring-1-simple-start/src/test/java/cn/argento/askia/SpringBeanTest.java
@@ -0,0 +1,12 @@
+package cn.argento.askia;
+
+
+import org.junit.Test;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+public class SpringBeanTest {
+
+    @Test
+    public void contextTest(){
+    }
+}
