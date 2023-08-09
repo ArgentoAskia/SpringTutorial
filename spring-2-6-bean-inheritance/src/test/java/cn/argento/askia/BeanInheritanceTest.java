9966c9b3506e6480469ac0697116ec1b894bc2f6
ArgentoAskia <askiaxiaoming@outlook.com>
2023-08-09 16:52:54 +0800

    7-2023.8.9: Spring Bean继承

---
+++ spring-2-6-bean-inheritance/src/test/java/cn/argento/askia/BeanInheritanceTest.java
@@ -0,0 +1,33 @@
+package cn.argento.askia;
+
+import cn.argento.askia.beans.DerivedTestBean;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanInheritanceTest {
+
+    private ClassPathXmlApplicationContext applicationContext;
+
+    @Before
+    public void before(){
+        applicationContext = new ClassPathXmlApplicationContext("classpath:spring-context.xml");
+        applicationContext.start();
+    }
+
+    @After
+    public void after(){
+        applicationContext.stop();
+        applicationContext.close();
+    }
+
+
+    @Test
+    public void testAbstractBeans(){
+        DerivedTestBean inheritsWithDifferentClass = applicationContext.getBean("inheritsWithDifferentClass", DerivedTestBean.class);
+        DerivedTestBean inheritsWithDifferentClass2 = applicationContext.getBean("inheritsWithDifferentClass2", DerivedTestBean.class);
+        System.out.println(inheritsWithDifferentClass);
+        System.out.println(inheritsWithDifferentClass2);
+    }
+}
